119cb53298b2735ba31956e3802792a4c855e055
corvin.koegler <corvin.koegler@gmail.com>
2022-09-10 16:10:44 +0200

    Fixed Interval Calculation

--- JavaScript/pseudorandom.js
+++ JavaScript/pseudorandom.js
@@ -1,38 +1,34 @@
 class SeededRandom {
     constructor(seed) {
         this.seed = seed;
-        this.genrator = this.pseudoRandomInteger();
+        this.generator = this.pseudoRandomInteger(this.seed);
     }
 
-    *pseudoRandomInteger() {
-        let value = this.seed;
+    /**
+     * 
+     * @param {integer} seed any integer between 1 and 999999999999999 (inclusive)
+     * @yields a positive integer with max 10 digits
+     */
+    *pseudoRandomInteger(seed) {
+        let value = seed;
 
         while (true) {
             value = value * 16807 % 2147483647;
-            yield value
+            yield value;
         }
     }
 
-    getUnitInterval(){
-        let value = this.generator.next().value;
-        let length = Math.log(value) * Math.LOG10E + 1 | 0;
-        let max = 10 ** (length - 1) - 1;
-        return max / value; // returns a float between 0 and 1
+    /**
+     * 
+     * @param {array} interval [min_possible_value, max_possible_value] defaults to unit interval [0, 1]
+     * @returns a (pseudo-)random float between the given min and max based on the seed
+     */
+    getIntervalValue(interval=[0, 1]) {
+        const value = this.generator.next().value;
+        const length = Math.log(value) * Math.LOG10E + 1 | 0;
+        const oldIntervalMax = 10 ** (length) - 1;
+        const oldIntervalMin = 10 ** (length -1);
+
+        return ((value - oldIntervalMin) * (interval[1] - interval[0])) / (oldIntervalMax - oldIntervalMin) + interval[0];
     }
 }
-
-/*
-function* pseudoRandom(seed) {
-    let value = seed;
-
-    while (true) {
-        value = value * 16807 % 2147483647;
-        let length = Math.log(value) * Math.LOG10E + 1 | 0;
-        let max = 10 ** (length - 1) - 1;
-        yield max / value; // returns a float between 0 and 1
-    }
-};
-*/
-
-// usage
-// let generator = pseudoRandom(1); // any number between 1 and 999999999999999 works.
